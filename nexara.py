fc5a021479f279d501276afb6d4628c642000098
zeidenweb <introuv69@gmail.com>
2024-08-18 03:39:53 +0200

    nexara.py

--- nexara.py
+++ nexara.py
@@ -4,21 +4,41 @@ from pystyle import Colors, Colorate
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def afficher_humouristique():
+    humouristique_titre = r"""
+         _nnnn_                      
+        dGGGGMMb     ,"""""""""""""""""".
+       @p~qp~~qMb    | Get Doxxed Nigga |
+       M|@||@) M|   _;..................'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--'  
+    """
+    print(Colorate.Horizontal(Colors.red_to_yellow, humouristique_titre))
+    input("Press Enter...")
+    clear_screen()  # Clear the screen after pressing Enter
+
 def afficher_titre():
     titre = """
-               
-
        ______    _     _             
       |___  /   (_)   | |            
          / / ___ _  __| | ___ _ __   
         / / / _ \ |/ _` |/ _ \ '_ \  
        / /_|  __/ | (_| |  __/ | | | 
       /_____\___|_|\__,_|\___|_| |_| 
-                                                                    
     """
     print(Colorate.Horizontal(Colors.red_to_yellow, titre))
 
-    welcome_message = "Welcome to Nexara the osint directory"
+    welcome_message = "Welcome to Nexara the OSINT directory V2"
     choose_option = "Choose an option :"
 
     print("\n" + welcome_message.center(os.get_terminal_size().columns))
@@ -42,42 +62,54 @@ def menu_principal():
 
 def menu_osint():
     print(Colorate.Horizontal(Colors.red_to_yellow, "Choose an option :"))
-    print(Colorate.Horizontal(Colors.blue_to_purple, "1. IP LOOKUP"))
+    print(Colorate.Horizontal(Colors.blue_to_purple, "1. IP Lookup"))
     print(Colorate.Horizontal(Colors.blue_to_purple, "2. Mail Lookup"))
     print(Colorate.Horizontal(Colors.blue_to_purple, "3. Username Lookup"))
     print(Colorate.Horizontal(Colors.blue_to_purple, "4. Image Lookup"))
     print(Colorate.Horizontal(Colors.blue_to_purple, "5. Number Lookup"))
     print(Colorate.Horizontal(Colors.blue_to_purple, "6. Free DB"))
-    print(Colorate.Horizontal(Colors.blue_to_purple, "7. Back to main menu"))
+    print(Colorate.Horizontal(Colors.blue_to_purple, "7. Google Advanced"))
+    print(Colorate.Horizontal(Colors.blue_to_purple, "8. Back to main menu"))
 
 def tool_info():
     print(Colorate.Horizontal(Colors.red_to_yellow, "Tool info:"))
-    print(Colorate.Horizontal(Colors.blue_to_purple, "created by zeiden "))
-    print(Colorate.Horizontal(Colors.blue_to_purple, "created on 01/08/24"))
+    print(Colorate.Horizontal(Colors.blue_to_purple, "Created by Zeiden"))
+    print(Colorate.Horizontal(Colors.blue_to_purple, "Created on 01/08/24 / update on 18/08/24"))
 
 def ip_lookup():
-    print(Colorate.Horizontal(Colors.red_to_yellow, "IP lookup & location : https://www.iplocation.net/ "))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "IP lookup & location : https://www.iplocation.net/"))
 
 def mail_lookup():
     print(Colorate.Horizontal(Colors.red_to_yellow, "Mail lookup : https://thatsthem.com/reverse-email-lookup"))
     print(Colorate.Horizontal(Colors.red_to_yellow, "https://epieos.com/"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://haveibeenpwned.com/"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://leakcheck.io/"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://dehashed.com/"))
 
 def username_lookup():
     print(Colorate.Horizontal(Colors.red_to_yellow, "Username lookup : https://whatsmyname.app/"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://instantusername.com/"))
 
 def image_lookup():
     print(Colorate.Horizontal(Colors.red_to_yellow, "Image lookup : https://tineye.com/"))
-    print(Colorate.Horizontal(Colors.red_to_yellow, "https://pimeyes.com/en"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://pimeyes.com/fr"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://facecheck.id/fr"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://lens.google/intl/fr/"))
 
 def numero_lookup():
     print(Colorate.Horizontal(Colors.red_to_yellow, "Numéro lookup : https://www.capeutservir.com/telephonie/"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://www.emobiletracker.com/"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "https://www.pagesjaunes.fr/annuaireinverse"))
+
+def google_advanced():
+    print(Colorate.Horizontal(Colors.red_to_yellow, "Do advanced research on Google : https://www.google.com/advanced_search"))
 
 def free_db():
-    print(Colorate.Horizontal(Colors.red_to_yellow, "Free DB ---) discord.gg/snusbase"))
+    print(Colorate.Horizontal(Colors.red_to_yellow, "Free DB ---) https://discord.com/invite/lookup"))
 
 def main():
-    clear_screen()
-    afficher_titre()
+    afficher_humouristique()  # Affiche l'humour uniquement au démarrage
+    afficher_titre()  # Affiche le titre principal
     while True:
         menu_principal()
         choix = input("Enter your choice : ")
@@ -120,6 +152,11 @@ def main():
                     free_db()
                     input("Press Enter to return to the OSINT screen ...")
                 elif sous_choix == '7':
+                    clear_screen()
+                    afficher_titre()
+                    google_advanced()
+                    input("Press Enter to return to the OSINT screen ...")
+                elif sous_choix == '8':
                     clear_screen()
                     afficher_titre()
                     break
